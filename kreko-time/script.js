2db137c5c4db98ce61497065a6bf85d322a7be7e
MareikeMKB <mareike.bitz@gmx.net>
2025-01-02 13:01:42 +0100

    stuff

---
+++ kreko-time/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// buttons
+document.querySelector("#button1").addEventListener("click", function () {
+    null;
+});
+document.querySelector("#button2").addEventListener("click", function () {
+    null;
+});
+document.querySelector("#button3").addEventListener("click", function () {
+    null;
+});
+document.querySelector("#button4").addEventListener("click", function () {
+    null;
+});
+document.querySelector("#button5").addEventListener("click", function () {
+    null;
+});
+document.querySelector("#start").addEventListener("click", function () {
+    null;
+});
+//# sourceMappingURL=script.js.map
