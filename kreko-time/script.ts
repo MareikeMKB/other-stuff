06a2c1c492f0ca4fc4615453ad20ce8cf3eefd22
MareikeMKB <mareike.bitz@gmx.net>
2025-01-02 22:19:29 +0100

    first part

--- kreko-time/script.ts
+++ kreko-time/script.ts
@@ -1,20 +1,67 @@
+// button switches
+let start= 1;
+let buttons = 1;
+
 // buttons
 document.querySelector("#button1")!.addEventListener("click", function () {
-    null;
+    if (buttons == 1) {
+        document.getElementById("zeit")!.removeChild(document.getElementById("present")!);
+        let img = document.createElement("img");
+        document.getElementById("zeit")!.appendChild(img);
+        img.setAttribute("src", "tunnel-14747_256.gif");
+        img.setAttribute("id", "future");
+        buttons = 0;
+    }
 })
 document.querySelector("#button2")!.addEventListener("click", function () {
-    null;
+    if (buttons == 1) {
+        document.getElementById("zeit")!.removeChild(document.getElementById("present")!);
+        let img = document.createElement("img");
+        document.getElementById("zeit")!.appendChild(img);
+        img.setAttribute("src", "feather-13992_256.gif");
+        img.setAttribute("id", "future");
+        buttons = 0;
+    }
 })
 document.querySelector("#button3")!.addEventListener("click", function () {
-    null;
+    if (buttons == 1) {
+        document.getElementById("zeit")!.removeChild(document.getElementById("present")!);
+        let img = document.createElement("img");
+        document.getElementById("zeit")!.appendChild(img);
+        img.setAttribute("src", "christmas-tree-15498_256.gif");
+        img.setAttribute("id", "future");
+        buttons = 0;
+    }
 })
 document.querySelector("#button4")!.addEventListener("click", function () {
-    null;
+    if (buttons == 1) {
+        document.getElementById("zeit")!.removeChild(document.getElementById("present")!);
+        let img = document.createElement("img");
+        document.getElementById("zeit")!.appendChild(img);
+        img.setAttribute("src", "sci-fi-16832_256.gif");
+        img.setAttribute("id", "future");
+        buttons = 0;
+    }
 })
 document.querySelector("#button5")!.addEventListener("click", function () {
-    null;
+    if (buttons == 1) {
+        document.getElementById("zeit")!.removeChild(document.getElementById("present")!);
+        let img = document.createElement("img");
+        document.getElementById("zeit")!.appendChild(img);
+        img.setAttribute("src", "tunnel-14747_256.gif");
+        img.setAttribute("id", "future");
+        buttons = 0;
+    }
 })
+
 document.querySelector("#start")!.addEventListener("click", function () {
-    null;
+    if (start == 1) {
+        document.getElementById("zeit")!.removeChild(document.getElementById("past")!);
+        let img = document.createElement("img");
+        document.getElementById("zeit")!.appendChild(img);
+        img.setAttribute("src", "716411778153709628.gif");
+        img.setAttribute("id", "present");
+        start = 0;
+    }
 })
 
