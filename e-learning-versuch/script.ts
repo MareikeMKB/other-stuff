d587e9e2809c7ddbd51eb47ad33798a784da127b
MareikeMKB <mareike.bitz@gmx.net>
2025-01-25 11:43:32 +0100

    functional

---
+++ e-learning-versuch/script.ts
@@ -0,0 +1,19 @@
+document.getElementById("linka1")!.addEventListener("click", function(){
+    window.location.href = "a1.html";
+})
+
+document.getElementById("linka2")!.addEventListener("click", function(){
+    window.location.href = "a2.html";
+})
+
+document.getElementById("linka3")!.addEventListener("click", function(){
+    window.location.href = "a3.html";
+})
+
+document.getElementById("linka4")!.addEventListener("click", function(){
+    window.location.href = "a4.html";
+})
+
+document.getElementById("linka5")!.addEventListener("click", function(){
+    window.location.href = "a5.html";
+})
